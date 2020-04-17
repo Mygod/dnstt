e5019355788dd3272036ba36bca38b07d890ec1d
David Fifield <david@bamsoftware.com>
2020-04-16 23:47:29 -0600

    Handle 429 Too Many Requests.

--- dnstt-client/http.go
+++ dnstt-client/http.go
@@ -8,14 +8,18 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"strconv"
+	"sync"
 	"time"
 
 	"www.bamsoftware.com/git/dnstt.git/turbotunnel"
 )
 
 type HTTPPacketConn struct {
-	urlString string
-	client    *http.Client
+	urlString     string
+	client        *http.Client
+	notBefore     time.Time
+	notBeforeLock sync.RWMutex
 	*turbotunnel.QueuePacketConn
 }
 
@@ -38,6 +42,20 @@ func NewHTTPPacketConn(urlString string, numSenders int) (*HTTPPacketConn, error
 		}()
 	}
 	return c, nil
+}
+
+func (c *HTTPPacketConn) WriteTo(p []byte, addr net.Addr) (int, error) {
+	// Drop packets while we are rate-limiting ourselves (as a result of a
+	// Retry-After response header, for example).
+	c.notBeforeLock.RLock()
+	notBefore := c.notBefore
+	c.notBeforeLock.RUnlock()
+	if time.Now().Before(notBefore) {
+		return len(p), nil
+	}
+
+	// Ignore addr.
+	return c.QueuePacketConn.WriteTo(p, dummyAddr{})
 }
 
 // send sends a single packet in an HTTP request.
@@ -66,14 +84,45 @@ func (c *HTTPPacketConn) send(p []byte) error {
 		}
 		// Ignore err != nil; don't report an error if we at least
 		// managed to send.
+	case http.StatusTooManyRequests:
+		// https://developers.google.com/speed/public-dns/docs/doh/#errors
+		now := time.Now()
+		var retryAfter time.Time
+		if value := resp.Header.Get("Retry-After"); value != "" {
+			var err error
+			retryAfter, err = parseRetryAfter(value, now)
+			if err != nil {
+				log.Printf("cannot parse Retry-After value %+q", value)
+			}
+		}
+		if retryAfter.IsZero() {
+			// Supply a default.
+			retryAfter = now.Add(60 * time.Second)
+		}
+		c.notBeforeLock.Lock()
+		if retryAfter.After(now) && retryAfter.After(c.notBefore) {
+			log.Printf("got %+q; ceasing sending for %v", resp.Status, retryAfter.Sub(now))
+			c.notBefore = retryAfter
+		}
+		c.notBeforeLock.Unlock()
 	default:
 		return fmt.Errorf("unknown HTTP response status %+q", resp.Status)
 	}
+
 	return nil
 }
 
-func (c *HTTPPacketConn) WriteTo(p []byte, addr net.Addr) (int, error) {
-	// TODO delay to Retry-After
-	// Ignore addr.
-	return c.QueuePacketConn.WriteTo(p, dummyAddr{})
+// parseRetryAfter parses the value of a Retry-After header as an absolute
+// time.Time.
+func parseRetryAfter(value string, now time.Time) (time.Time, error) {
+	// May be a date string or an integer number of seconds.
+	// https://tools.ietf.org/html/rfc7231#section-7.1.3
+	if t, err := http.ParseTime(value); err == nil {
+		return t, nil
+	}
+	i, err := strconv.ParseUint(value, 10, 32)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return now.Add(time.Duration(i) * time.Second), nil
 }
